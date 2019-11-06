853374ebad099042f596070e72291881dcb90050
Enamul Haque <ehaque95@gmail.com>
2019-11-06 10:26:04 +0600

    coding test

---
+++ src/practice/Count_and_Say.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practice;
+
+/**
+ *
+ * @author enamul
+ */
+public class Count_and_Say {
+    
+    public int seq(int n){
+     
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
